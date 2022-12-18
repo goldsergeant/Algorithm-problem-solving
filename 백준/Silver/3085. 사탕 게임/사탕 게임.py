2d7d3d9489877d29f12eb5d0c1ae14b99be7528b
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2022-12-18 23:00:18 +0900

    [Silver III] Title: 사탕 게임, Time: 1536 ms, Memory: 119800 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -4,72 +4,46 @@ n = int(input())
 arr = collections.deque()
 for _ in range(n):
     arr.append(list(input()))
-answer=0
+result = []
 
 
-def check(arr):
-    n = len(arr)
-    answer = 1
-
+def check():
+    num = 1
     for i in range(n):
-        # 열 순회하면서 연속되는 숫자 세기
-        cnt = 1
-        for j in range(1, n):
-            if arr[i][j] == arr[i][j - 1]:
-                # 이전 것과 같다면 cnt에 1 더하기
-                cnt += 1
-            else:
-                # 이전과 다르다면 다시 1로 초기화
-                cnt = 1
-
-            # 비교해서 현재 cnt가 더 크다면 answer 갱신하기
-            if cnt > answer:
-                answer = cnt
-
-        # 행 순회하면서 연속되는 숫자 세기
-        cnt = 1
-        for j in range(1, n):
-            if arr[j][i] == arr[j - 1][i]:
-                # 이전 것과 같다면 cnt에 1 더하기
-                cnt += 1
-            else:
-                # 이전과 다르다면 다시 1로 초기화
-                cnt = 1
-
-            # 비교해서 현재 cnt가 더 크다면 answer 갱신하기
-            if cnt > answer:
-                answer = cnt
-
-    return answer
-
+        for j in range(1,n):
+            if arr[i][j-1]==arr[i][j]:
+                num+=1
+            elif arr[i][j-1]!=arr[i][j]:
+                num=1
+            if num not in result:
+                result.append(num)
+        num=1
+        for j in range(1,n):
+            if arr[j-1][i]==arr[j][i]:
+                num+=1
+            elif arr[j-1][i]!=arr[j][i]:
+                num=1
+            if num not in result:
+                result.append(num)
+        num=1
 
 for i in range(n):
     for j in range(n):
-        # 열 바꾸기
-        if j + 1 < n:
-            # 인점한 것과 바꾸기
-            arr[i][j], arr[i][j + 1] = arr[i][j + 1], arr[i][j]
-
-            # check는 arrd에서 인점한 것과 바꿨을 때 가장 긴 연속한 부분을 찾아내는 함수이다
-            temp = check(arr)
-
-            if temp > answer:
-                answer = temp
-
-            # 바꿨던 것을 다시 원래대로 돌려놓기
-            arr[i][j], arr[i][j + 1] = arr[i][j + 1], arr[i][j]
-
-        # 행 바꾸기
-        if i + 1 < n:
-            # 인점한 것과 바꾸기
-            arr[i][j], arr[i + 1][j] = arr[i + 1][j], arr[i][j]
-
-            # check는 arrd에서 인점한 것과 바꿨을 때 가장 긴 연속한 부분을 찾아내는 함수이다
-            temp = check(arr)
-
-            if temp > answer:
-                answer = temp
-
-            # 바꿨던 것을 다시 원래대로 돌려놓기
-            arr[i][j], arr[i + 1][j] = arr[i + 1][j], arr[i][j]
-print(answer)
+        if j > 0 and arr[i][j-1]!=arr[i][j]:
+            arr[i][j - 1], arr[i][j] = arr[i][j], arr[i][j - 1]
+            check()
+            arr[i][j - 1], arr[i][j] = arr[i][j], arr[i][j - 1]
+        if j < n - 1 and arr[i][j+1]!=arr[i][j]:
+            arr[i][j + 1], arr[i][j] = arr[i][j], arr[i][j + 1]
+            check()
+            arr[i][j + 1], arr[i][j] = arr[i][j], arr[i][j + 1]
+        if i < n - 1 and arr[i+1][j]!=arr[i][j]:
+            arr[i + 1][j], arr[i][j] = arr[i][j], arr[i + 1][j]
+            check()
+            arr[i + 1][j], arr[i][j] = arr[i][j], arr[i + 1][j]
+        if i > 0 and arr[i-1][j]!=arr[i][j]:
+            arr[i - 1][j], arr[i][j] = arr[i][j], arr[i - 1][j]
+            check()
+            arr[i - 1][j], arr[i][j] = arr[i][j], arr[i - 1][j]
+check()
+print(max(result) if len(result)>0 else n)
