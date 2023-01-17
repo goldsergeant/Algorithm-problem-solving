9180d7b23660dd1958faa762968cea01a43bc417
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-01-17 09:14:53 +0900

    [Bronze III] Title: 직각삼각형, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+import sys
+
+while True:
+    a,b,c=map(int,sys.stdin.readline().split())
+    if a==b==c==0:
+        break
+    if a**2+b**2==c**2 or a**2+c**2==b**2 or b**2+c**2==a**2:
+        print('right')
+    else:
+        print('wrong')
