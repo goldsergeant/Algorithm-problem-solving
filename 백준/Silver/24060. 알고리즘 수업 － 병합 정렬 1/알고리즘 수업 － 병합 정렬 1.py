10627da962bedb0d7eca3c9a98116f73760b9eba
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-01-19 20:27:30 +0900

    [Silver IV] Title: 알고리즘 수업 - 병합 정렬 1, Time: 1092 ms, Memory: 325828 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -2,14 +2,16 @@ import sys
 
 a_size, k = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
-answer=[]
+answer = []
+
+
 def merge(p, q, r):
-    if r-p==0:
+    if r - p == 0:
         return
     i = p
     j = q + 1
     t = 0
-    tmp = [0 for i in range(r-p+1)]
+    tmp = [0 for i in range(r - p + 1)]
     while i <= q and j <= r:
         if arr[i] <= arr[j]:
             tmp[t] = arr[i]  # tmp[t] <- A[i]; t++; i++;
@@ -33,21 +35,20 @@ def merge(p, q, r):
     while i <= r:  # 결과를 A[p..r]에 저장
         arr[i] = tmp[t]
         answer.append(tmp[t])
-        if len(tmp)==k:
+        if len(tmp) == k:
             print(tmp[t])
             return
         i += 1
         t += 1
 
 
-
 def merge_sort(p, r):
     if p < r:
         q = (p + r) // 2
-        merge_sort( p, q)
-        merge_sort( q + 1, r)
-        merge( p, q, r)
+        merge_sort(p, q)
+        merge_sort(q + 1, r)
+        merge(p, q, r)
 
 
-merge_sort(0, len(arr)-1)
-print(answer[k-1] if len(answer)>=k else -1)
+merge_sort(0, len(arr) - 1)
+print(answer[k - 1] if len(answer) >= k else -1)
