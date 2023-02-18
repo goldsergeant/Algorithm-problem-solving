5a94988b40d3a7c6fffec6f116f7665eedaa11ff
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-02-18 14:23:46 +0900

    [Gold III] Title: 소수의 연속합, Time: 1840 ms, Memory: 96416 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -9,13 +9,14 @@ for i in range(2, n + 1):
         for j in range(2 * i, n + 1, i):
             a[j] = False
 
+total=0
+end=0
 for i in range(len(prime_numbers)):
-    total = n
-    for j in range(i, len(prime_numbers)):
-        total-=prime_numbers[j]
-        if total==0:
-            answer+=1
-        elif total<0:
-            break
+    while total<n and end<len(prime_numbers):
+        total+=prime_numbers[end]
+        end+=1
+    if total==n:
+        answer+=1
+    total-=prime_numbers[i]
 
 print(answer)
