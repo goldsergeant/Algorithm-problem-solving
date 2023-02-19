b958a662f827244e5c0086c4bf31761c145807ad
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-02-19 19:18:16 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,21 +1,25 @@
+import collections
 def solution(tickets):
-    tickets.sort(key=lambda x:(x[0],x[1]))
-    global check
-    check=False
-    answer=[]
-    visited=[False for i in range(len(tickets))]
-    def dfs(node,depth):
-        global check
-        if depth==len(tickets):
-            check=True
-        answer.append(node)
+    # 1. 그래프 생성
+    graph = collections.defaultdict(list)
 
-        for i in range(len(tickets)):
-            if not visited[i] and tickets[i][0]==node:
-                visited[i]=True
-                dfs(tickets[i][1],depth+1)
-                if not check:
-                    answer.pop()
-                    visited[i]=False
-    dfs("ICN",0)
-    return answer
+    for (start, end) in tickets:
+        graph[start].append(end)
+
+        # 2. 시작점 - [끝점] 역순으로 정렬
+    for node in graph.keys():
+        graph[node].sort(reverse=True)
+
+    # 3. DFS 알고리즘으로 path를 만들어줌.
+    stack=collections.deque()
+    stack.append("ICN")
+    answer=[]
+
+    while stack:
+        next=stack[-1]
+
+        if graph[next]==[]:
+            answer.append(stack.pop())
+        else:
+            stack.append(graph[next].pop())
+    return answer[::-1]
