2d08404f2cdd8272353ee4791083a8b1f4984b52
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-03-16 20:55:09 +0900

    [Gold IV] Title: 운동, Time: 2680 ms, Memory: 116848 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,27 +1,20 @@
-import collections
-import heapq
 import sys
 
 v,e=map(int,sys.stdin.readline().split())
-graph=collections.defaultdict(list)
 heap=[]
 distance=[[sys.maxsize for _ in range(v+1)] for _ in range(v+1)]
 for _ in range(e):
     a,b,c=map(int,sys.stdin.readline().split())
-    graph[a].append((b,c))
-    heapq.heappush(heap,(c,a,b))
     distance[a][b]=c
 
-while heap:
-    c,a,b=heapq.heappop(heap)
-    if a==b:
-        print(c)
-        exit()
-    if c>distance[a][b]:
-        continue
+for k in range(1,v+1):
+    for i in range(1,v+1):
+        for j in range(1,v+1):
+            if distance[i][j]>distance[i][k]+distance[k][j]:
+                distance[i][j]=distance[i][k]+distance[k][j]
 
-    for next_vertex,next_dist in graph[b]:
-        if distance[a][next_vertex]>next_dist+c:
-            distance[a][next_vertex]=next_dist+c
-            heapq.heappush(heap,(distance[a][next_vertex],a,next_vertex))
-print(-1)
+answer=sys.maxsize
+for i in range(1,v+1):
+    answer=min(answer,distance[i][i])
+
+print(answer if answer!=sys.maxsize else -1)
