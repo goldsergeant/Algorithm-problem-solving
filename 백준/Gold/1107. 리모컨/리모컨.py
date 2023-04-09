d24b30630ed75bd85f81060767608313113293f7
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-04-09 14:40:50 +0900

    [Gold V] Title: 리모컨, Time: 200 ms, Memory: 115308 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -2,16 +2,16 @@ import sys
 
 n=int(input())
 m=int(input())
-break_buttons=[]
+broken_buttons=[]
 if m!=0:
-    break_buttons=list(map(int,sys.stdin.readline().split()))
+    broken_buttons=list(sys.stdin.readline().split())
 cur_channel=100
 answer=abs(cur_channel-n)
 
 for i in range(1000001):
     flag=0
-    for break_button in break_buttons:
-        if str(break_button) in str(i):
+    for break_button in broken_buttons:
+        if break_button in str(i):
             flag=1
             break
     if flag==1:
