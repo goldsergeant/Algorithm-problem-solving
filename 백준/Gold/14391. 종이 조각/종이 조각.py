23929adfc7778531a3e216481c9f817062abc850
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-04-22 13:10:37 +0900

    [Gold III] Title: 종이 조각, Time: 304 ms, Memory: 33852 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.py
+++ 백준/Gold/14391. 종이 조각/종이 조각.py
@@ -1,41 +1,50 @@
-import sys
+import sys #백트래킹으로 풀기
 
 n, m = map(int, sys.stdin.readline().split())
 arr = []
-check_list = [[0 for _ in range(m)] for _ in range(n)]
 answer = []
 for _ in range(n):
-    arr.append(list(map(int,sys.stdin.readline().rstrip())))
+    arr.append(list(map(int, sys.stdin.readline().rstrip())))
+check = [[False for _ in range(m)] for _ in range(n)]
 
 
-def check_sum():
-    for i in range(1 << (n * m)):
-        total = 0
+def dfs(i, j):
+    if i==n:
+        answer.append(calc())
+        return
 
+    if j==m:
+        dfs(i+1,0)
+        return
+
+    check[i][j]=True
+    dfs(i,j+1)
+    check[i][j]=False
+    dfs(i,j+1)
+
+
+def calc() -> int:
+    total = 0
+    for row in range(n):
+        row_sum = 0
+        for col in range(m):  # 가로 연산 체크되어있으면 쭉
+            if check[row][col]:
+                row_sum = row_sum * 10 + arr[row][col]
+            else:
+                total += row_sum
+                row_sum = 0
+        total += row_sum
+
+    for col in range(m):
+        col_sum = 0
         for row in range(n):
-            row_sum = 0
-            for col in range(m):
-                idx = row * m + col
-                if i & (1 << idx) == 0:  # 가로 연산
-                    row_sum = row_sum * 10 + arr[row][col]
-                else:
-                    total += row_sum
-                    row_sum = 0
-            total += row_sum
+            if not check[row][col]:
+                col_sum = col_sum * 10 + arr[row][col]
+            else:
+                total += col_sum
+                col_sum = 0
+        total += col_sum
+    return total
 
-        for col in range(m):
-            col_sum = 0
-            for row in range(n):
-                idx = row * m + col
-                if i & (1 << idx) != 0: #세로 연산
-                    col_sum = col_sum * 10 + arr[row][col]
-                else:
-                    total += col_sum
-                    col_sum = 0
-            total += col_sum
-        answer.append(total)
-
-
-check_sum()
-
+dfs(0,0)
 print(max(answer))
