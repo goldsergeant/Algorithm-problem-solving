a315a8dc6379ab53a43466465f90f11f11e8fd11
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-06-25 18:25:51 +0900

    [Gold IV] Title: 좀비 떼가 기관총 진지에도 오다니, Time: 2412 ms, Memory: 270716 KB -BaekjoonHub

--- 백준/Gold/19644. 좀비 떼가 기관총 진지에도 오다니/좀비 떼가 기관총 진지에도 오다니.py
+++ 백준/Gold/19644. 좀비 떼가 기관총 진지에도 오다니/좀비 떼가 기관총 진지에도 오다니.py
@@ -6,57 +6,20 @@ Ml,Mk=map(int,input().split())
 C=int(input())
 zombies=[int(sys.stdin.readline().rstrip()) for _ in range(L)]
 answer=True
-queue=collections.deque()
-use_bomb=0
 
-for i in range(min(Ml,L)): #처음 큐에 넣기
-    if use_bomb==0:
-        expect_zombie_health=zombies[i]-Mk*(i+1)
-        if expect_zombie_health<=0:
-            queue.append(0)
-        else:
-            queue.append(expect_zombie_health)
-            use_bomb+=1
+deq=collections.deque()
 
+for i in range(L):
+    while deq and deq[0]<=i:
+        deq.popleft()
+
+    total_damage=(len(deq)+1)*Mk
+    if total_damage>=zombies[i]:
+        deq.append(i+Ml)
+    elif C>0:
+        C-=1
     else:
-        expect_zombie_health=zombies[i]-Mk*(i+1-use_bomb)
-        if expect_zombie_health<=0:
-            queue.append(0)
-        else:
-            queue.append(expect_zombie_health)
-            use_bomb+=1
+        answer=False
+        break
 
-for i in range(Ml,L):
-    first_zombie=queue.popleft()
-    if first_zombie==0:
-        if zombies[i]-Mk*(Ml-use_bomb)<=0:
-            queue.append(0)
-        else:
-            queue.append(zombies[i]-Mk*(Ml-use_bomb))
-            use_bomb+=1
-
-    else:
-        if C>0:
-            C-=1
-        else:
-            answer=False
-            break
-        if zombies[i]-Mk*(Ml-use_bomb)<=0:
-            queue.append(0)
-            use_bomb-=1
-        else:
-            queue.append(zombies[i]-Mk*(Ml-use_bomb))
-
-if answer:
-    while queue:
-        first_zombie=queue.popleft()
-
-        if first_zombie==0:
-            continue
-        else:
-            if C>0:
-                C-=1
-            else:
-                answer=False
-                break
 print('YES' if answer else 'NO')
