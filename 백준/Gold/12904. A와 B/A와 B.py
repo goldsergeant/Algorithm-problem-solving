cc7f00eddc0adc23f14a184e7e865894594ed2aa
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-06-26 09:25:09 +0900

    [Gold V] Title: A와 B, Time: 72 ms, Memory: 34236 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,13 @@
+import collections
+
+s=input()
+t=input()
+
+while len(t)>len(s):
+    if t[-1]=='A':
+        t=t[:-1]
+    elif t[-1]=='B':
+        t=t[:-1]
+        t=t[::-1]
+
+print(1 if s==t else 0)
