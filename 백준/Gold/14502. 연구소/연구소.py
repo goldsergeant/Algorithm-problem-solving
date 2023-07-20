7c3d5758303c19792854732a4f66d4ac1f9ac000
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-07-20 14:47:46 +0900

    [Gold IV] Title: 연구소, Time: 4036 ms, Memory: 31868 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,44 @@
+import copy
+import itertools
+import sys
+
+n,m=map(int,input().split())
+board=[]
+dy=[-1,1,0,0]
+dx=[0,0,-1,1]
+zero_index=[]
+answer=[]
+
+for _ in range(n):
+    board.append(list(map(int,sys.stdin.readline().split())))
+
+def dfs(row, col, clone_board):
+    for i in range(4):
+        ny=row+dy[i]
+        nx=col+dx[i]
+        if ny<0 or nx<0 or ny>n-1 or nx>m-1:
+            continue
+        if clone_board[ny][nx]==0:
+            clone_board[ny][nx]=2
+            dfs(ny, nx, clone_board)
+
+for i in range(n):
+    for j in range(m):
+        if board[i][j]==0:
+            zero_index.append((i,j))
+
+for combi in itertools.combinations(zero_index,3):
+    clone_board=copy.deepcopy(board)
+    one=combi[0]
+    two=combi[1]
+    three=combi[2]
+    clone_board[one[0]][one[1]]=1
+    clone_board[two[0]][two[1]]=1
+    clone_board[three[0]][three[1]]=1
+    for i in range(n):
+        for j in range(m):
+            if clone_board[i][j]==2:
+                dfs(i,j,clone_board)
+    answer.append(sum(i.count(0) for i in clone_board))
+
+print(max(answer))
