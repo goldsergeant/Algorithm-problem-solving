cb318a93e8812f17d8b0d8f0d3e588ffed3c3e35
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-07-20 14:53:22 +0900

    [Gold IV] Title: 연구소, Time: 2476 ms, Memory: 31712 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -8,6 +8,7 @@ dy=[-1,1,0,0]
 dx=[0,0,-1,1]
 zero_index=[]
 answer=[]
+virus=[]
 
 for _ in range(n):
     board.append(list(map(int,sys.stdin.readline().split())))
@@ -26,6 +27,8 @@ for i in range(n):
     for j in range(m):
         if board[i][j]==0:
             zero_index.append((i,j))
+        elif board[i][j]==2:
+            virus.append((i,j))
 
 for combi in itertools.combinations(zero_index,3):
     clone_board=copy.deepcopy(board)
@@ -35,10 +38,8 @@ for combi in itertools.combinations(zero_index,3):
     clone_board[one[0]][one[1]]=1
     clone_board[two[0]][two[1]]=1
     clone_board[three[0]][three[1]]=1
-    for i in range(n):
-        for j in range(m):
-            if clone_board[i][j]==2:
-                dfs(i,j,clone_board)
+    for v in virus:
+        dfs(v[0],v[1],clone_board)
     answer.append(sum(i.count(0) for i in clone_board))
 
 print(max(answer))
