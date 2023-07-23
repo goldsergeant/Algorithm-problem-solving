2fc8c9a77659d4bc6f97c233b731f5d4b086c74a
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-07-23 14:47:41 +0900

    [Gold V] Title: 조 짜기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2229. 조 짜기/조 짜기.py
+++ 백준/Gold/2229. 조 짜기/조 짜기.py
@@ -1,11 +1,15 @@
 import sys
 
-n=int(input())
-students=list(map(int,sys.stdin.readline().split()))
-students.insert(0,0)
-dp=[0 for _ in range(10001)]
-max_diff=0
-for i in range(1,n+1):
-    for j in range(i,0,-1):
-        dp[i]=max(dp[i],max(students[j:i+1])-min(students[j:i+1])+dp[j-1])
-print(dp[n])
+n = int(input())
+students =list(map(int, sys.stdin.readline().split()))
+dp = [0 for _ in range(n + 1)]
+for i in range(n):
+    dp[i+1]=dp[i]
+    maxi=mini=students[i]
+    j=i-1
+    while j>=0 and (students[j]<mini or students[j]>maxi):
+        mini,maxi=min(mini,students[j]),max(maxi,students[j]),
+        dp[i+1]=max(dp[i+1],dp[j]+maxi-mini)
+        j-=1
+
+print(dp[-1])
