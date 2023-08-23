a4f867c25d427bfda9275130b94ad3de346534d0
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-08-23 17:16:31 +0900

    [Gold III] Title: 방 번호, Time: 64 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Gold/1082. 방 번호/방 번호.py
+++ 백준/Gold/1082. 방 번호/방 번호.py
@@ -4,33 +4,10 @@ import sys
 n=int(input())
 p=list(map(int,input().split()))
 m=int(input())
-spent_money=0
-answer=[]
+dp=[0 for _ in range(m+1)]
 
-min_idx=p.index(min(p))
-if min_idx==0:
-    if n==1:
-        print(0)
-        exit()
-    min_idx_not0=p.index(min(p[1:]))
-    if m<p[min_idx_not0]:
-        print(0)
-        exit()
+for i in range(n-1,-1,-1):
+    for j in range(p[i],m+1):
+        dp[j]=max(dp[j],dp[j-p[i]]*10+i,i)
 
-    answer.append((min_idx_not0,p[min_idx_not0]))
-    spent_money+=p[min_idx_not0]
-
-while spent_money+p[min_idx]<=m:
-    answer.append((min_idx,p[min_idx]))
-    spent_money+=p[min_idx]
-
-for i in range(len(answer)):
-    room,price=answer[i]
-    for j in range(n-1,-1,-1):
-        if spent_money+p[j]-price<=m:
-            answer[i]=(j,p[j])
-            spent_money+=(p[j]-price)
-            break
-
-print(''.join(str(i[0]) for i in answer))
-
+print(dp[m])
