a06aa8afd4e2786f532ca339483ee5cc6d1c657d
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-10-15 12:39:05 +0900

    [Gold IV] Title: 여행 가자, Time: 72 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -0,0 +1,28 @@
+import collections
+import sys
+
+n=int(sys.stdin.readline())
+m=int(sys.stdin.readline())
+parent=[i for i in range(n+1)]
+
+def find(x):
+    if parent[x]!=x:
+        return find(parent[x])
+    return x
+
+def union(a,b):
+    a=find(a)
+    b=find(b)
+    if a!=b:
+        if a<b:
+            parent[b]=a
+        else:
+            parent[a]=b
+for i in range(1,n+1):
+    link_info=[0]+list(map(int,sys.stdin.readline().split()))
+    for j in range(1,n+1):
+        if link_info[j]==1:
+            union(i,j)
+
+plan=list(map(int,sys.stdin.readline().split()))
+print('YES') if len(set(find(i) for i in plan))==1 else print('NO')
