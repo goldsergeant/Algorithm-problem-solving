8c6d9db6742b96f52f9aa1573f1002302ab1d535
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-12-26 19:58:08 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 1324 ms, Memory: 121288 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -0,0 +1,43 @@
+import sys
+
+
+def my_input():
+    return sys.stdin.readline().rstrip()
+
+
+N = int(my_input())
+eggs = []
+answer = 0
+for _ in range(N):
+    s, w = map(int, my_input().split())
+    eggs.append([s, w])
+
+
+def smash_eggs(i, j):
+    eggs[i][0] -= eggs[j][1]
+    eggs[j][0] -= eggs[i][1]
+
+
+def recover_eggs(i, j):
+    eggs[i][0] += eggs[j][1]
+    eggs[j][0] += eggs[i][1]
+
+
+def dfs(left):
+    global answer
+    answer = max(answer, len(list(filter(lambda x: x[0] <= 0, eggs))))
+    if left == N:
+        return
+
+    if eggs[left][0]>0:
+        for i in range(N):
+            if i!=left and eggs[i][0]>0:
+                smash_eggs(left,i)
+                dfs(left+1)
+                recover_eggs(left,i)
+    else:
+        dfs(left+1)
+
+
+dfs(0)
+print(answer)
