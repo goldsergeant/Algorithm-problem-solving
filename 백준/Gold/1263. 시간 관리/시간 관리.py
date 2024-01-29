5dd297dff5f88365dc5631d6677ae885e3e14cf8
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2024-01-29 22:05:54 +0900

    [Gold V] Title: 시간 관리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1263. 시간 관리/시간 관리.py
+++ 백준/Gold/1263. 시간 관리/시간 관리.py
@@ -4,16 +4,16 @@ N=int(sys.stdin.readline())
 tasks=[]
 for _ in range(N):
     t,s=map(int,sys.stdin.readline().split())
-    tasks.append((t,s))
+    tasks.append((s-t,s))
 
-tasks.sort(reverse=True,key=lambda x:(x[1],-x[0]))
-start_time=tasks[0][1]-tasks[0][0]
+tasks.sort(reverse=True,key=lambda x:(x[1],x[0]))
+start_time=tasks[0][0]
 for i in range(1,N):
-    cur_dur,cur_end=tasks[i]
+    cur_start,cur_end=tasks[i]
     if start_time<cur_end:
-        start_time=(cur_end-cur_dur)-(cur_end-start_time)
+        start_time=cur_start-(cur_end-start_time)
     else:
-        start_time=cur_end-cur_dur
+        start_time=cur_start
 
 print(start_time if start_time>=0 else -1)
 
