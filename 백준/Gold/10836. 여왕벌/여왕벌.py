91a720a68f6fcc927ddc942faa18de0ba844dd21
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2024-02-26 22:47:12 +0900

    [Gold IV] Title: 여왕벌, Time: 2704 ms, Memory: 112268 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -1,22 +1,15 @@
 import sys
 
 M, N = map(int, sys.stdin.readline().split())
-board = [[1 for _ in range(M)] for _ in range(M)]
+grow = [1 for _ in range(2 * M - 1)]
+
 for _ in range(N):
-    arr = list(map(int, sys.stdin.readline().split()))
-    row, col = M - 1, 0
-    for i in range(len(arr)):
-        while arr[i]>0:
-            board[row][col] += i
-            arr[i]-=1
-            if row != 0:
-                row -= 1
-            else:
-                col += 1
+    a, b, c = map(int, sys.stdin.readline().split())
+    for i in range(a, a + b):
+        grow[i] += 1
+    for i in range(a + b, a + b + c):
+        grow[i] += 2
 
-for i in range(1,M):
-    for j in range(1,M):
-        board[i][j]=max(board[i-1][j],board[i-1][j-1],board[i][j-1])
+for i in range(M - 1, -1, -1):
+    print(*([grow[i]] + grow[M:]))
 
-for arr in board:
-    print(' '.join(map(str,arr)))
