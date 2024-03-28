cfa2734d95ed9fcefea9fcdf787c42dc7574dc7e
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2024-03-28 22:11:04 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 660 ms, Memory: 158960 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.py
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.py
@@ -4,24 +4,17 @@ import sys
 board = [list(sys.stdin.readline().rstrip()) for _ in range(8)]
 dr = [0, 0, 1, -1, 1, -1, 1, -1, 0]
 dc = [1, -1, 0, 0, 1, 1, -1, -1, 0]
-blocks = []
-for i in range(8):
-    for j in range(8):
-        if board[i][j] == '#':
-            blocks.append((i, j))
-
 
 def move_blocks():
-    for i in range(len(blocks)):
-        r, c = blocks[i]
-        blocks[i]=(r+1,c)
+    board.pop()
+    board.insert(0,['.' for _ in range(8)])
 def bfs():
     q = collections.deque([(7, 0)])
     second = 0
     while q:
         for _ in range(len(q)):
             r, c = q.popleft()
-            if (r,c) in blocks:
+            if board[r][c]=='#':
                 continue
             if r == 0 or second == 9:
                 return 1
@@ -29,7 +22,7 @@ def bfs():
             for i in range(len(dr)):
                 nr, nc = dr[i] + r, dc[i] + c
                 if 0 <= nr < 8 and 0 <= nc < 8:
-                    if (nr,nc) not in blocks:
+                    if board[nr][nc]!='#':
                         q.append((nr, nc))
         move_blocks()
         second += 1
