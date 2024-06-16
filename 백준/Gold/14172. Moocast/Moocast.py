05a4ff9f9d2936fb16d8331451eae06ffbc8f5ec
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2024-06-16 21:13:43 +0900

    [Gold V] Title: Moocast, Time: 152 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Gold/14172. Moocast/Moocast.py
@@ -0,0 +1,39 @@
+import collections
+import sys
+
+def bfs(idx):
+    cnt=0
+    q=collections.deque([idx])
+    visited=[False for _ in range(N)]
+    visited[idx]=True
+    while q:
+        i=q.popleft()
+        x1,y1,p1=cows[i]
+        cnt+=1
+        for j in range(N):
+            x2,y2,p2=cows[j]
+            distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2)
+            if not visited[j] and distance<=p1:
+                visited[j]=True
+                q.append(j)
+
+    return cnt
+
+
+N=int(sys.stdin.readline())
+cows=[[*map(int,sys.stdin.readline().split())] for _ in range(N)]
+answer=0
+graph=[[] for _ in range(N)]
+for i in range(N):
+    x1,y1,p1=cows[i]
+    for j in range(i+1,N):
+        x2,y2,p2=cows[j]
+        distance=((x2-x1)**2+(y2-y1)**2)**(1/2)
+        if distance<=p1:
+            graph[i].append(j)
+        if distance<=p2:
+            graph[j].append(i)
+
+for i in range(N):
+    answer=max(answer,bfs(i))
+print(answer)
