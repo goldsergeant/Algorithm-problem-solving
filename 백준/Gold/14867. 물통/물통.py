8bf78a098b823b08ee46bd89d5ab4ee9a53ad0a3
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2024-06-18 16:54:45 +0900

    [Gold II] Title: 물통, Time: 860 ms, Memory: 85756 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14867. 물통/물통.py
+++ 백준/Gold/14867. 물통/물통.py
@@ -1,37 +1,51 @@
+import collections
 import sys
-sys.setrecursionlimit(1000000)
-
-def dfs(a,b):
-    if a==a2 and b==b2:
-        return 0
-    if dp[a][b]!=-1:
-        return dp[a][b]
-
-    dp[a][b]=sys.maxsize
-    if a<a1: # 물통 채우는 단계
-        dp[a][b]=min(dp[a][b],dfs(a1,b)+1)
-    if b<b1:
-        dp[a][b]=min(dp[a][b],dfs(a,b1)+1)
-
-    if a>0: # 물통 버리는 단계
-        dp[a][b]=min(dp[a][b],dfs(0,b)+1)
-    if b>0:
-        dp[a][b]=min(dp[a][b],dfs(a,0)+1)
-
-    if b<b1: # 물통 옮기는 단계
-        if a<=b1-b:
-            dp[a][b]=min(dp[a][b],dfs(0,b+a)+1)
-        else:
-            dp[a][b]=min(dp[a][b],dfs(a-(b1-b),b1)+1)
-    if a<a1:
-        if b<=a1-a:
-            dp[a][b]=min(dp[a][b],dfs(a+b,0)+1)
-        else:
-            dp[a][b]=min(dp[a][b],dfs(a1,b-(a1-a))+1)
-
-    return dp[a][b]
 
 
 a1,b1,a2,b2=map(int,sys.stdin.readline().split())
-dp=[[-1 for _  in range(b1+1)] for _ in range(a1+1)]
-print(dfs(0,0) if dfs(0,0)!=sys.maxsize else -1)
+q=collections.deque([(0,0,0)])
+visited=set()
+while q:
+    a,b,cnt=q.popleft()
+    if a==a2 and b==b2:
+        print(cnt)
+        exit()
+    if a < a1:  # 물통 채우는 단계
+        if (a1,b) not in visited:
+            visited.add((a1,b))
+            q.append((a1,b,cnt+1))
+    if b < b1:
+        if (a,b1) not in visited:
+            visited.add((a,b1))
+            q.append((a,b1,cnt+1))
+
+    if a > 0:  # 물통 버리는 단계
+        if (0,b) not in visited:
+            visited.add((0,b))
+            q.append((0,b,cnt+1))
+
+    if b > 0:
+        if (a,0) not in visited:
+            visited.add((a,0))
+            q.append((a,0,cnt+1))
+
+    if b < b1:  # 물통 옮기는 단계
+        if a <= b1 - b:
+            if (0,b+a) not in visited:
+                visited.add((0,b+a))
+                q.append((0,b+a, cnt + 1))
+        else:
+            if (a-(b1-b),b1) not in visited:
+                visited.add((a-(b1-b),b1))
+                q.append((a-(b1-b),b1, cnt + 1))
+    if a < a1:
+        if b <= a1 - a:
+            if (a+b,0) not in visited:
+                visited.add((a+b,0))
+                q.append((a+b,0, cnt + 1))
+        else:
+            if (a1,b-(a1-a)) not in visited:
+                visited.add((a1,b-(a1-a)))
+                q.append((a1,b-(a1-a), cnt + 1))
+
+print(-1)
