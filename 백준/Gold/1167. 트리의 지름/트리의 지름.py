7c2e3639997aeca48b59fd00063542186ae97c1e
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2024-09-04 18:48:13 +0900

    [Gold II] Title: 트리의 지름, Time: 580 ms, Memory: 92608 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,31 +1,36 @@
 import collections
 import sys
+sys.setrecursionlimit(1000000)
 
-v=int(sys.stdin.readline())
-graph=collections.defaultdict(list)
-max_node=max_distance=0
+N = int(sys.stdin.readline())
+tree = collections.defaultdict(list)
+for _ in range(N):
+    arr = list(map(int, sys.stdin.readline().split()))
+    node = arr[0]
+    for i in range(1, len(arr)-1, 2):
+        tree[node].append((arr[i],arr[i+1]))
 
-for _ in range(v):
-    arr=list(map(int,sys.stdin.readline().split()))
-    node=arr[0]
-    for i in range(1,len(arr)-1,2):
-        graph[node].append((arr[i],arr[i+1]))
+visited=[False for _ in range(N+1)]
+long_tup=(0,0)
 
-def dfs(node,total_distance):
-    global max_distance,max_node
-
-    visited[node]=True
-    if max_distance<total_distance:
-        max_distance=total_distance
-        max_node=node
-
-    for next_node,next_distance in graph[node]:
+def get_long_node(node,distance):
+    global long_tup
+    if distance>long_tup[1]:
+        long_tup=(node,distance)
+    visited[node] = True
+    for next_node,next_distance in tree[node]:
         if not visited[next_node]:
-            dfs(next_node,total_distance+next_distance)
+            get_long_node(next_node,distance+next_distance)
+def get_distance(node):
+    visited[node]=True
+    max_distance=0
+    for next_node,next_distance in tree[node]:
+        if not visited[next_node]:
+            max_distance=max(max_distance, get_distance(next_node) + next_distance)
 
-visited=[False]*(v+1)
-dfs(1,0)
-visited=[False]*(v+1)
-max_distance=0
-dfs(max_node,0)
-print(max_distance)
+    return max_distance
+
+get_long_node(1,0)
+# print(long_tup)
+visited=[False for _ in range(N+1)]
+print(get_distance(long_tup[0]))
