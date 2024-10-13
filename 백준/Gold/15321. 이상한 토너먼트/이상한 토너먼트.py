aed6b0b8e977cd8b817837377a51361714338d07
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2024-10-13 23:01:47 +0900

    [Gold II] Title: 이상한 토너먼트, Time: 580 ms, Memory: 118584 KB -BaekjoonHub

--- 백준/Gold/15321. 이상한 토너먼트/이상한 토너먼트.py
+++ 백준/Gold/15321. 이상한 토너먼트/이상한 토너먼트.py
@@ -1,24 +1,22 @@
 import sys
-from heapq import heappop,heappush
-N = int(sys.stdin.readline())
-tournament = [int(sys.stdin.readline()) for _ in range(N)]
-heap=[]
-for power in tournament:
-    heappush(heap,power)
+from functools import cache
 
-answer=0
-while len(heap)>1:
-    worst_val=heappop(heap)
-    worst_idx=tournament.index(worst_val)
-    if worst_idx==0:
-        right=tournament[worst_idx+1]
-        answer+=abs(worst_val-right)
-    elif worst_idx==len(tournament)-1:
-        left=tournament[worst_idx-1]
-        answer+=abs(worst_val-left)
-    else:
-        left=tournament[worst_idx-1]
-        right=tournament[worst_idx+1]
-        answer+=min(abs(worst_val-left),abs(worst_val-right))
-    tournament.pop(worst_idx)
-print(answer)
+N=int(sys.stdin.readline())
+coding_powers=[int(sys.stdin.readline()) for _ in range(N)]
+dp=[[0 for _ in range(N)] for _ in range(N)]
+winner=[[0 for _ in range(N)] for _ in range(N)]
+for i in range(N):
+    winner[i][i] = coding_powers[i]
+    for j in range(i+1,N):
+        winner[i][j] = max(winner[i][j-1], coding_powers[j])
+
+
+
+for i in range(N-1,-1,-1):
+    for j in range(i+1,N):
+        tmp=sys.maxsize
+        for k in range(i,j):
+            tmp=min(tmp,dp[i][k]+dp[k+1][j]+abs(winner[i][k]-winner[k+1][j]))
+        dp[i][j]=tmp
+
+print(dp[0][N-1])
