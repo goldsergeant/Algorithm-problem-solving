2b31475dded9eff42177560dda33170cf533ed3b
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2024-10-18 00:04:35 +0900

    [level 2] Title: 하노이의 탑, Time: 24.90 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,15 +1,14 @@
 def solution(n):
-    answer = []
     
     def hanoi(num,a_tower,b_tower):
         if num==1:
-            answer.append([a_tower,b_tower])
+            yield [a_tower,b_tower]
             return
         
         c_tower=6-a_tower-b_tower
-        hanoi(num-1,a_tower,c_tower)
-        answer.append([a_tower,b_tower])
-        hanoi(num-1,c_tower,b_tower)
+        yield from hanoi(num-1,a_tower,c_tower)
+        yield [a_tower,b_tower]
+        yield from hanoi(num-1,c_tower,b_tower)
     
-    hanoi(n,1,3)
-    return answer
+    return list(hanoi(n,1,3))
+    
