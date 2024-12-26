9853062ea71b709026a916aeb9c2a462ac1acc1a
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2024-12-26 14:07:53 +0900

    [level 3] Title: 거스름돈, Time: 2094.76 ms, Memory: 268 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.py
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -7,8 +7,8 @@ def solution(n, money):
     for i in range(1,len(money)):
         for j in range(n+1):
             if j-money[i]>=0:
-                dp[i][j]=dp[i-1][j]+dp[i][j-money[i]]
+                dp[i][j]=(dp[i-1][j]+dp[i][j-money[i]])%1000000007
             else:
-                dp[i][j]=dp[i-1][j]
+                dp[i][j]=dp[i-1][j]%1000000007
     
     return dp[len(money)-1][n]
