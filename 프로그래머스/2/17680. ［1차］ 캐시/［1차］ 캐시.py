33317518d06c7a66619b41ac017140f206efb0d5
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-01-15 13:56:29 +0900

    [level 2] Title: [1차] 캐시, Time: 75.99 ms, Memory: 24 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,33 +1,18 @@
-from heapq import heappush, heappop
+import collections
+
 
 def solution(cacheSize, cities):
     answer = 0
-    cache=dict()
+    cache=collections.deque(maxlen=cacheSize)
     cities=list(map(lambda x:x.lower(),cities))
 
     for idx,city in enumerate(cities):
         if city in cache:
-            answer += 1
-            cache[city]=len(cities)-idx
+            answer+=1
+            cache.remove(city)
+            cache.append(city)
         else:
             answer+=5
-            if len(cache)<cacheSize:
-                cache[city]=len(cities)-idx
-            else:
-                remove_cache_city=''
-                recent_use_time=0
-                for cache_city,time in cache.items():
-                    if cache_city in cities:
-                        if time>recent_use_time:
-                            recent_use_time=time
-                            remove_cache_city=cache_city
-
-                if cacheSize>0:
-                    cache.pop(remove_cache_city)
-                    cache[city]=len(cities)-idx
+            cache.append(city)
 
     return answer
-
-print(solution(3,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
-print(solution(3,["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
-print(solution(0,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
