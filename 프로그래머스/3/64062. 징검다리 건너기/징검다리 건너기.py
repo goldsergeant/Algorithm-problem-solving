22e099abdabcce824c3018e0fdfb1efe6d0c991c
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-01-15 12:06:42 +0900

    [level 3] Title: 징검다리 건너기, Time: 526.32 ms, Memory: 18.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,29 @@
+def solution(stones, k):
+    def check(num) -> bool:
+        num-=1
+        cnt = 0
+        for stone in stones:
+            if stone - num <= 0:
+                cnt += 1
+                if cnt == k:
+                    return False
+            else:
+                cnt = 0
+
+        return True
+
+    left = 1
+    right = sum(stones)+max(stones)
+
+    while left + 1 < right:
+        mid = (left + right) // 2
+        if check(mid):
+            left = mid
+        else:
+            right = mid
+
+    return left
+
+
+# print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3))
+print(solution([5],5))
