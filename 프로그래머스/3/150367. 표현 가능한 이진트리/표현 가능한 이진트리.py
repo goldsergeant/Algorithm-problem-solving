1e8583e77a6d687d17002ab7f831bdef350c72f1
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-02-09 16:14:57 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 128.05 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,26 +1,38 @@
 def solution(numbers):
-    global flag
-
-    def preorder(left,right):
-        global flag
-        if flag and left<right:
-            mid = (left+right)//2
-            if binary[mid] == '0' and '1' in binary[left:right+1]:
-                flag = False
-            else:
-                preorder(left,mid-1)
-                preorder(mid+1,right)
-
+    global is_possible
     answer = []
+    is_possible = True
+    tree_length = [1]
+    factor = 2
+    while tree_length[-1] <= 10000:
+        tree_length.append(tree_length[-1] + factor)
+        factor *= 2
+
+    for i in range(len(numbers)):
+        l = len(bin(numbers[i])[2:])
+        numbers[i] = bin(numbers[i])[2:]
+        for j in range(1, len(tree_length)):
+            if tree_length[j - 1] < l <= tree_length[j]:
+                l = tree_length[j]
+                numbers[i] = numbers[i].zfill(l)
+                break
+
+    def preorder(num, left, right):
+        global is_possible
+        if left == right:
+            return num[left]
+
+        mid = (left + right) // 2
+        l = preorder(num, left, mid - 1)
+        r = preorder(num, mid + 1, right)
+        if num[mid] == '0':
+            if l=='1' or r=='1':
+                is_possible = False
+
+        return num[mid]
+
     for num in numbers:
-        flag = True
-        binary = bin(num)[2:]
-        target_len = 1
-        while len(binary) >= target_len:
-            target_len *= 2
-
-        binary = binary.zfill(target_len - 1)
-        preorder(0,target_len-1)
-        answer.append(1 if flag else 0)
-
+        is_possible = True
+        preorder(num, 0, len(num) - 1)
+        answer.append(int(is_possible))
     return answer
