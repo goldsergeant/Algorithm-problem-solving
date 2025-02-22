48c83063ff2f33bc651bd0aab5def8d0b7665baa
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-02-22 14:18:51 +0900

    [Gold IV] Title: 배수 공사, Time: 2200 ms, Memory: 168488 KB -BaekjoonHub

--- 백준/Gold/15817. 배수 공사/배수 공사.py
+++ 백준/Gold/15817. 배수 공사/배수 공사.py
@@ -2,13 +2,13 @@ import sys
 
 N, X = map(int, sys.stdin.readline().split())
 pipes = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-dp=[[0 for _ in range(X+1)] for _ in range(N+1)]
-dp[0][0]=1
-for i in range(N):
-    for j in range(X+1):
-        for k in range(pipes[i][1]+1):
-            next_length=j+pipes[i][0]*k
-            if next_length<=X:
-                dp[i+1][next_length]+=dp[i][j]
+dp = [[0 for _ in range(X + 1)] for _ in range(N + 1)]
+dp[0][0] = 1
+for i in range(1, N + 1):
+    for j in range(X + 1):
+        for k in range(pipes[i-1][1] + 1):
+            pre_length = j-pipes[i-1][0]*k
+            if pre_length >= 0:
+                dp[i][j] += dp[i-1][pre_length]
 
 print(dp[-1][X])
