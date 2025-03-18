16abb58f434c1c03553a72d095abdeef116c7974
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-03-18 09:59:31 +0900

    [Gold III] Title: 등수 찾기, Time: 716 ms, Memory: 167368 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17616. 등수 찾기/등수 찾기.py
+++ 백준/Gold/17616. 등수 찾기/등수 찾기.py
@@ -1,52 +1,40 @@
 import collections
 import sys
 
-N,M,X=map(int,sys.stdin.readline().split())
-graph1=collections.defaultdict(list)
-graph2=collections.defaultdict(list)
-in_degree1=[0 for _ in range(N+1)]
-in_degree2=[0 for _ in range(N+1)]
-rank1=[0 for _ in range(N+1)]
-rank2=[0 for _ in range(N+1)]
+N, M, X = map(int, sys.stdin.readline().split())
+graph1 = collections.defaultdict(list)
+graph2 = collections.defaultdict(list)
+
+behind_nodes = 0
+front_nodes = 0
+
 
 for _ in range(M):
-    a,b=map(int,sys.stdin.readline().split())
+    a, b = map(int, sys.stdin.readline().split())
     graph1[b].append(a)
-    in_degree1[a]+=1
-
     graph2[a].append(b)
-    in_degree2[b]+=1
 
-q1=collections.deque()
-q2=collections.deque()
-for i in range(1,N+1):
-    if in_degree1[i]==0:
-        q1.append(i)
-    if in_degree2[i]==0:
-        q2.append(i)
+q1=collections.deque([X])
+q2=collections.deque([X])
 
-cur_rank=N
+visited=[False for _ in range(N+1)]
+visited[X]=True
 while q1:
-    for _ in range(len(q1)):
-        node=q1.popleft()
-        rank1[node]=cur_rank
-        for next_node in graph1[node]:
-            in_degree1[next_node]-=1
-            if in_degree1[next_node]==0:
-                q1.append(next_node)
+    node = q1.popleft()
+    for n in graph1[node]:
+        if not visited[n]:
+            front_nodes += 1
+            q1.append(n)
+            visited[n]=True
 
-        cur_rank-=1
-
-cur_rank=1
+visited=[False for _ in range(N+1)]
+visited[X]=True
 while q2:
-    for _ in range(len(q2)):
-        node=q2.popleft()
-        rank2[node]=cur_rank
-        for next_node in graph2[node]:
-            in_degree2[next_node]-=1
-            if in_degree2[next_node]==0:
-                q2.append(next_node)
+    node=q2.popleft()
+    for n in graph2[node]:
+        if not visited[n]:
+            behind_nodes += 1
+            q2.append(n)
+            visited[n]=True
 
-        cur_rank+=1
-
-print(rank2[X],rank1[X])
+print(1+front_nodes, N-behind_nodes)
