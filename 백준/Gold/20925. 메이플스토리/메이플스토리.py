2933fe54cf823fe5890670efb4616408848c416f
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-03-21 12:47:52 +0900

    [Gold II] Title: 메이플스토리, Time: 1384 ms, Memory: 112660 KB -BaekjoonHub

--- 백준/Gold/20925. 메이플스토리/메이플스토리.py
+++ 백준/Gold/20925. 메이플스토리/메이플스토리.py
@@ -1,26 +1,24 @@
 import sys
 
-N,T=map(int,sys.stdin.readline().split())
-exp_infos=[list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-move_time=[list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-dp=[[0 for _ in range(N)] for _ in range(T+1)] # 시간,노드
-
-# for i in range(N):
-#     if exp_infos[i][0]==0:
-#         dp[0][i]=exp_infos[i][1]
-
-for t in range(1,T+1):
+N, T = map(int, sys.stdin.readline().split())
+fields = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+move_times = list(list(map(int, sys.stdin.readline().split())) for _ in range(N))
+dp = [[0 for _ in range(T + 1)] for _ in range(N)] # 사냥터, 시간
+for time in range(1,T+1):
     for i in range(N):
-        # dp[t][i]=dp[t-1][i]+exp_infos[i][1]
+        if dp[i][time-1]>=fields[i][0]:
+            dp[i][time]=dp[i][time-1]+fields[i][1]
         for j in range(N):
             if i==j:
-                if dp[t-1][i]>=exp_infos[i][0]:
-                    dp[t][i]=max(dp[t][i],dp[t-1][i]+exp_infos[i][1])
-            else:
-                prev_time=t-move_time[i][j]
-                if prev_time>=0 and dp[prev_time][j]-exp_infos[j][1]>=exp_infos[i][0]:
-                    dp[t][i]=max(dp[t][i],dp[prev_time][j]+exp_infos[i][1]-exp_infos[j][1])
+                continue
+            move_time=move_times[i][j]
+            if time-move_time>=0 and dp[j][time-move_time]-fields[j][1]>=fields[i][0]:
+                dp[i][time]=max(dp[i][time],dp[j][time-move_time]-fields[j][1]+fields[i][1])
 
-# for arr in dp:
-#     print(arr)
-print(max(dp[T]))
+
+answer=0
+for i in range(N):
+    answer=max(answer,dp[i][T])
+print(answer)
+
+
