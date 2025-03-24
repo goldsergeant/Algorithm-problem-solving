f17432b9f55ef7ee8eff9183904d6069c4126597
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-03-24 10:27:11 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 624 ms, Memory: 274660 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.py
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.py
@@ -1,29 +1,24 @@
-import collections
 import sys
 
+
+def convert_task_to_bit(task):
+    return 1 << task
+
+
 N = int(sys.stdin.readline())
-cost = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-dp = [[-1 for _ in range(1 << N)] for _ in range(N)]  # 사람, 방문 비트
+costs = [list(map(int, sys.stdin.readline().split())) for i in range(N)]
+dp = [[sys.maxsize for _ in range(1 << N)] for _ in range(N)]  # 사람,일 비트
 
+for task in range(N):
+    task_bit = convert_task_to_bit(task)
+    dp[0][task_bit] = min(dp[0][task_bit], costs[0][task])
 
-def dfs(cur_person, visit_tasks):
-    if visit_tasks == (1 << N) - 1:
-        return 0
-    if dp[cur_person][visit_tasks] != -1:
-        return dp[cur_person][visit_tasks]
+for i in range(1,N):
+    for bit in range(1,1<<N):
+        if dp[i-1][bit]!=sys.maxsize:
+            for task in range(N):
+                task_bit = convert_task_to_bit(task)
+                if not bit & task_bit:
+                    dp[i][bit|task_bit]=min(dp[i][bit|task_bit],dp[i-1][bit]+costs[i][task])
 
-    dp[cur_person][visit_tasks] = sys.maxsize
-
-    for next_task in range(N):
-        if visit_tasks & (1 << next_task) == 0:  # 방문 체크
-            dp[cur_person][visit_tasks] = min(dp[cur_person][visit_tasks],
-                                              dfs(cur_person + 1,
-                                                  visit_tasks | (1 << next_task)) + cost[cur_person + 1][
-                                                  next_task])
-    return dp[cur_person][visit_tasks]
-
-
-answer = sys.maxsize
-for i in range(N):
-    answer = min(answer, dfs(0, 1 << i)+cost[0][i])
-print(answer)
+print(dp[-1][-1])
