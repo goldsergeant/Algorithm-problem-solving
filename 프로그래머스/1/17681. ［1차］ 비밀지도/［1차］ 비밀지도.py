e114a08fbd529955d970db0b319299897f1851c2
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-03-25 16:17:09 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.08 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,20 +1,16 @@
 def solution(n, arr1, arr2):
     answer = []
-    board1=[[] for _ in range(n)]
-    board2=[[] for _ in range(n)]
+    board=[0 for _ in range(n)]
     for i in range(n):
-        board1[i]=list(map(int,bin(arr1[i])[2:].zfill(n)))
-        board2[i]=list(map(int,bin(arr2[i])[2:].zfill(n)))
-
+        board[i]=arr1[i]|arr2[i]
+        
     for i in range(n):
-        arr=[]
+        tmp=[]
         for j in range(n):
-            if board1[i][j] or board2[i][j]:
-                arr.append('#')
+            if 1<<j&board[i]:
+                tmp.append('#')
             else:
-                arr.append(' ')
-        answer.append(''.join(arr))
+                tmp.append(' ')
+        board[i]=''.join(tmp[::-1])
+    return board
 
-    return answer
-
-print(solution(6,[46, 33, 33 ,22, 31, 50],[27 ,56, 19, 14, 14, 10]))
