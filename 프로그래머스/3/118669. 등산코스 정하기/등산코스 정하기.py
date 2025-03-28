8208dc1d6da87080109e0a0e4bd2533e1f80489e
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-03-28 16:07:56 +0900

    [level 3] Title: 등산코스 정하기, Time: 747.76 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,37 +1,54 @@
 import collections
-from heapq import heappush,heappop
 import sys
-            
+from heapq import heappop,heappush
+
 def solution(n, paths, gates, summits):
-    answer = [sys.maxsize,sys.maxsize]
-    graph=[[] for _ in range(n+1)]
-    distance=[sys.maxsize for _ in range(n+1)]
+
     is_summit=[False for _ in range(n+1)]
-    for summit in summits:
-        is_summit[summit]=True
-        
-    heap=[]
+    for s in summits:
+        is_summit[s]=True
+    graph=collections.defaultdict(list)
     for u,v,c in paths:
         graph[u].append((v,c))
         graph[v].append((u,c))
-        
-    for gate in gates:
-        heappush(heap,(0,0,gate))
-        distance[gate]=0
-    
+
+    heap = []
+
+    for g in gates:
+        heappush(heap,(0,0,g))
+
+    distance = [[sys.maxsize, sys.maxsize] for _ in range(n + 1)]
+    target_intensity = sys.maxsize
+    target_summit = sys.maxsize
     while heap:
-        cost,max_intensity,node=heappop(heap)
-        max_intensity=max(max_intensity,cost)
-        if max_intensity>distance[node]:
-            continue
+        weight, visited_summit, node = heappop(heap)
         if is_summit[node]:
-            if max_intensity<answer[1] or (max_intensity==answer[1] and node<answer[0]):
-                answer=[node,max_intensity]
+            if visited_summit:
+                if target_intensity > weight:
+                    target_intensity = weight
+                    target_summit = visited_summit
+                elif target_intensity == weight and target_summit > visited_summit:
+                    target_summit = visited_summit
+                continue
+        if distance[node][bool(visited_summit)] < weight:
             continue
-            
-        for next_node,next_cost in graph[node]:
-            if max(next_cost,max_intensity)<distance[next_node]:
-                distance[next_node]=max(next_cost,max_intensity)
-                heappush(heap,(next_cost,max_intensity,next_node))
-                
-    return answer
+        for adj, adj_weight in graph[node]:
+            n_weight = max(weight, adj_weight)
+            if is_summit[adj]:
+                if visited_summit:
+                    continue
+                else:
+                    if distance[adj][1] > n_weight:
+                        distance[adj][1] = n_weight
+                        heappush(heap, (n_weight, adj, adj))
+            else:
+                if distance[adj][bool(visited_summit)] > n_weight:
+                    distance[adj][bool(visited_summit)] = n_weight
+                    heappush(heap, (n_weight, visited_summit, adj))
+
+    return [target_summit,target_intensity]
+
+
+print(solution(	6, [[1, 2, 3], [2, 3, 5], [2, 4, 2], [2, 5, 4], [3, 4, 4], [4, 5, 3], [4, 6, 1], [5, 6, 1]], [1, 3], [5]))
+print(
+    solution(	7, [[1, 4, 4], [1, 6, 1], [1, 7, 3], [2, 5, 2], [3, 7, 4], [5, 6, 6]], [1], [2, 3, 4]))
