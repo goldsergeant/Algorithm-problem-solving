3d5ad73c4fc6e04eba0f92108440b716b6fece6d
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-04-03 13:51:54 +0900

    [Gold II] Title: 도토리 숨기기, Time: 76 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.py
+++ 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.py
@@ -1,27 +1,25 @@
 import sys
 
-def check(box):
-    cnt = 0
-    for start,end,space in rules:
-        if box < start:
-            continue
 
-        cnt += (min(end, box) - start) // space + 1
+def check(box_limit):
+    cnt=0
+
+    for start,end,space in rules:
+        if box_limit<start:
+            continue
+        cnt+=(min(end,box_limit)-start)//space+1
 
     return cnt>=D
 
 N,K,D=map(int,sys.stdin.readline().split())
-rules=[]
-total_acorn=0
-for _ in range(K):
-    start,end,space=map(int,sys.stdin.readline().split())
-    rules.append([start,end,space])
-    total_acorn+=(end-start)//space+1
+rules=[list(map(int,sys.stdin.readline().split())) for _ in range(K)]
 
 left=1
 right=N
+
 while left+1<right:
     mid=(left+right)//2
+
     if check(mid):
         right=mid
     else:
