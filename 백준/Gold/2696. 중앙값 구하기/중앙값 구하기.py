36a75bfa1d33ecfa3af933f1323dcbddef70a17a
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-04-03 10:43:18 +0900

    [Gold II] Title: 중앙값 구하기, Time: 212 ms, Memory: 113552 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -1,30 +1,35 @@
 import sys
 import math
-from bisect import bisect_left
+from heapq import heappush, heappop
 
-T=int(sys.stdin.readline())
+T = int(sys.stdin.readline())
 for _ in range(T):
-    M=int(sys.stdin.readline())
-    numbers=[]
-    for _ in range(math.ceil(M/10)):
-        numbers.extend(list(map(int,sys.stdin.readline().split())))
+    M = int(sys.stdin.readline())
+    numbers = []
+    for _ in range(math.ceil(M / 10)):
+        numbers.extend(list(map(int, sys.stdin.readline().split())))
 
-    arr=[]
-    answer=[]
+    max_heap = []
+    min_heap = []
+    answer = []
 
-    for i,num in enumerate(numbers):
-        if not arr or arr[-1]<=num:
-            arr.append(num)
+    for i, num in enumerate(numbers):
+        if len(max_heap) == len(min_heap):
+            heappush(max_heap, -num)
         else:
-            idx=bisect_left(arr,num)
-            arr.insert(idx,num)
+            heappush(min_heap, num)
 
+        if min_heap and min_heap[0] < -max_heap[0]:
+            big = -heappop(max_heap)
+            small = heappop(min_heap)
+            heappush(min_heap,big)
+            heappush(max_heap, -small)
         if i%2==0:
-            answer.append(arr[len(arr)//2])
+            answer.append(-max_heap[0])
 
     print(len(answer))
     for i in range(len(answer)):
-        print(answer[i],end=' ')
-        if (i+1)%10==0 and i<len(answer)-1:
+        print(answer[i], end=' ')
+        if (i + 1) % 10 == 0 and i < len(answer) - 1:
             print()
     print()
