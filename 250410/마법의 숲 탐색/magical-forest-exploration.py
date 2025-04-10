41711441febfe047d25f335416cd8e0973e90e53
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-04-10 16:14:34 +0900

    Title: 마법의 숲 탐색, Time: 88ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 250410/마법의 숲 탐색/magical-forest-exploration.py
+++ 250410/마법의 숲 탐색/magical-forest-exploration.py
@@ -10,6 +10,7 @@ golem_move_dir = [(1, 0), (0, -1), (0, 1)]
 
 BOARD_EMPTY = 0
 BOARD_EXIT = 1
+BOARD_GOLEM=2
 
 
 def get_golem_points(center_r, center_c):  # 골렘의 위치들을 구하는 함수
@@ -26,7 +27,9 @@ def is_top_out_golem(center_r, center_c):  # 골렘이 위쪽에 걸터있을때
 def place_golem(center_r,center_c, exit_state):
     points = get_golem_points(center_r, center_c)
     for r, c in points:
-        board[r][c] = cur_golem_num
+        board[r][c] = BOARD_GOLEM
+        golem_num_board[r][c]=cur_golem_num
+
     if exit_state == EXIT_IS_NORTH:
         board[center_r - 1][center_c] = BOARD_EXIT
     elif exit_state == EXIT_IS_EAST:
@@ -79,8 +82,11 @@ def move_golem_south(center_r, center_c):
 
     for r, c in have_to_move_points:
         tmp_board[r][c] = BOARD_EMPTY
+        golem_num_board[r][c]=0
     for r, c in have_to_move_points:
         tmp_board[r + 1][c] = board[r][c]
+        golem_num_board[r+1][c]=cur_golem_num
+
     board = tmp_board
     return True
 
@@ -92,8 +98,10 @@ def move_golem_west(center_r, center_c, exit_state):
     have_to_move_points = get_golem_points(center_r, center_c)
     for r, c in have_to_move_points:
         tmp_board[r][c] = BOARD_EMPTY
+        golem_num_board[r][c]=0
     for r, c in have_to_move_points:
-        tmp_board[r + 1][c - 1] = cur_golem_num
+        tmp_board[r + 1][c - 1] = BOARD_GOLEM
+        golem_num_board[r+1][c-1]=cur_golem_num
 
     tmp_center_r, tmp_center_c = center_r + 1, center_c - 1
     if exit_state == EXIT_IS_NORTH:  # 출구가 반시계방향으로 바뀜
@@ -119,8 +127,10 @@ def move_golem_east(center_r, center_c, exit_state):
     have_to_move_points = get_golem_points(center_r, center_c)
     for r, c in have_to_move_points:
         tmp_board[r][c] = BOARD_EMPTY
+        golem_num_board[r][c]=0
     for r, c in have_to_move_points:
-        tmp_board[r + 1][c + 1] = cur_golem_num
+        tmp_board[r + 1][c + 1] = BOARD_GOLEM
+        golem_num_board[r+1][c+1]=cur_golem_num
 
     tmp_center_r, tmp_center_c = center_r + 1, center_c + 1
     if exit_state == EXIT_IS_NORTH:  # 출구가 시계방향으로 바뀜
@@ -140,13 +150,13 @@ def move_golem_east(center_r, center_c, exit_state):
 
 
 def elf_bfs(s_r, s_c): # 6,3 주의
-    q = collections.deque([(s_r, s_c, cur_golem_num)])
+    q = collections.deque([(s_r, s_c)])
     visited = [[False for _ in range(C)] for _ in range(R)]
     last_r = 0
     while q:
-        r, c, golem_num = q.popleft()
+        r, c= q.popleft()
         last_r=max(r,last_r)
-        for dr, dc in [(0, -1), (0, 1), (1, 0)]:  # 동,남,서로만 감
+        for dr, dc in [(0, -1), (0, 1), (1, 0),(-1,0)]:
             nr, nc = r + dr, c + dc
             if nr < 0 or nc < 0 or nr >= R or nc >= C:
                 continue
@@ -155,12 +165,15 @@ def elf_bfs(s_r, s_c): # 6,3 주의
             if board[nr][nc] == BOARD_EMPTY:
                 continue
 
-            if board[nr][nc] == golem_num or board[nr][nc] == BOARD_EXIT:
+            if golem_num_board[nr][nc] == golem_num_board[r][c]:
+                visited[nr][nc]=True
+                q.append((nr, nc))
+            else:
+                if board[r][c]!=BOARD_EXIT:
+                    continue
+
                 visited[nr][nc] = True
-                q.append((nr, nc, golem_num))
-            elif board[nr][nc] != cur_golem_num and board[r][c] == BOARD_EXIT and cur_exit_state!=EXIT_IS_NORTH:
-                visited[nr][nc] = True
-                q.append((nr, nc, board[nr][nc]))
+                q.append((nr, nc))
 
     return last_r
 
@@ -173,6 +186,7 @@ def clear_board():
 R, C, K = map(int, input().split())
 R += 3
 board = [[BOARD_EMPTY for _ in range(C)] for _ in range(R)]  # 실제로 보드는 2열부터 시작한다고 가정
+golem_num_board=[[0 for _ in range(C)] for _ in range(R)]
 answer = 0
 cur_golem_num = 2
 for _ in range(K):
