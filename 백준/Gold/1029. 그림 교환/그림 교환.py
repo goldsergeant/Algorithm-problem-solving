664e93325e49febeb8f07dd90e95f09830a91a78
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-04-19 10:28:03 +0900

    [Gold I] Title: 그림 교환, Time: 704 ms, Memory: 149376 KB -BaekjoonHub

--- 백준/Gold/1029. 그림 교환/그림 교환.py
+++ 백준/Gold/1029. 그림 교환/그림 교환.py
@@ -1,21 +1,26 @@
 import sys
 
-def dfs(last_person,last_price,people_bit):
-    if dp[last_person][last_price][people_bit]!=-1:
-        return dp[last_person][last_price][people_bit]
-
-    dp[last_person][last_price][people_bit]=1
-
-    for i in range(N):
-        if (1<<i)&people_bit:
-            continue
-        if last_price<=prices[last_person][i]:
-            n_bit=people_bit|(1<<i)
-            dp[last_person][last_price][people_bit]=max(dp[last_person][last_price][people_bit],dfs(i,prices[last_person][i],n_bit)+1)
-
-    return dp[last_person][last_price][people_bit]
-
 N=int(sys.stdin.readline())
-prices=[list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
-dp=[[[-1 for _ in range(1<<N)] for _ in range(9+1)] for _ in range(N)] # 마지막 구매자,마지막 가격, 현재 산 사람들
-print(dfs(0,0,1))
+prices=[list(map(int,list(sys.stdin.readline().strip()))) for _ in range(N)]
+dp=[[[0 for _ in range(1<<N)] for _ in range(9+1)] for _ in range(N)] # 마지막 사람, 마지막 가격, 비트
+
+dp[0][0][1]=1
+answer=1
+
+for bit in range(1<<N):
+    for i in range(N):
+        i_bit=1<<i
+        for j in range(N):
+            j_bit=1<<j
+            for p in range(9+1):
+                if dp[i][p][bit]==0:
+                    continue
+                if bit&i_bit and bit&j_bit==0 and p<=prices[i][j]:
+                    n_bit=bit|j_bit
+                    dp[j][prices[i][j]][n_bit]=max(dp[j][prices[i][j]][n_bit],dp[i][p][bit]+1)
+
+for i in range(N):
+    for p in range(9+1):
+        answer=max(answer,max(dp[i][p]))
+
+print(answer)
