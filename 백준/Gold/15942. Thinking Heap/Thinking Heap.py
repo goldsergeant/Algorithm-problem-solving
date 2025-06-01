ab9c7990c8f232c6d2dcb8c4f3d9da968db88cd7
Hanlim Geum <94740533+goldsergeant@users.noreply.github.com>
2025-06-01 13:35:23 +0900

    [Gold II] Title: Thinking Heap, Time: 164 ms, Memory: 45856 KB -BaekjoonHub

--- 백준/Gold/15942. Thinking Heap/Thinking Heap.py
+++ 백준/Gold/15942. Thinking Heap/Thinking Heap.py
@@ -3,60 +3,70 @@ sys.setrecursionlimit(10**7)
 
 N = int(sys.stdin.readline())
 K, P = map(int, sys.stdin.readline().split())
+up_num = K + 1
+down_num = K - 1
+heap_num = 0
 heap = [0 for _ in range(N + 1)]
-upnum,downnum=K+1,K-1
-heapnum=0
+
 
 def raise_exception():
     print(-1)
     exit()
 
+
 def set_child(idx):
-    global downnum
-    downnum+=1
-    if downnum>N:
+    global down_num
+    if idx > N:
+        return
+
+    down_num += 1
+    if down_num > N:
         raise_exception()
 
-    heap[idx]=downnum
-    if idx*2<=N:
-        set_child(idx * 2)
-    if idx*2+1<=N:
-        set_child(idx * 2 + 1)
+    heap[idx] = down_num
+    set_child(idx * 2)
+    set_child(idx * 2 + 1)
+
 
 def set_parent(idx):
-    global upnum
-    upnum-=1
-    if upnum==0:
+    global up_num
+    if idx < 1:
+        return
+    up_num -= 1
+    if up_num == 0:
         raise_exception()
 
-    heap[idx]=upnum
-    if idx//2>0:
-        set_parent(idx//2)
+    heap[idx] = up_num
+    set_parent(idx // 2)
+
 
 def up_heap(idx):
     if idx==1:
         return
 
-    if heap[idx]<heap[idx//2]:
+    if heap[idx]>heap[idx//2]:
         heap[idx],heap[idx//2]=heap[idx//2],heap[idx]
         up_heap(idx//2)
 
 def insert(idx):
-    global heapnum
-    heapnum+=1
-    if heapnum==upnum:
-        heapnum=downnum+1
+    global heap_num
+    if idx > N:
+        return
 
-    heap[idx]=heapnum
-    up_heap(idx)
-    if idx*2<=N:
-        insert(idx*2)
-    if idx*2+1<=N:
-        insert(idx*2 + 1)
+    heap_num += 1
+    if heap_num == up_num:
+        heap_num = down_num + 1
+
+    heap[idx] = heap_num
+    insert(idx * 2)
+    insert(idx * 2 + 1)
+
 
 set_child(P)
 set_parent(P)
-for i in range(1,N+1):
-    if heap[i]==0:
+
+for i in range(1, N + 1):
+    if heap[i] == 0:
         insert(i)
+
 print(*heap[1:],sep='\n')
